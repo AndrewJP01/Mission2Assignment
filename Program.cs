a29f40adf04d7bad8373f71d15a1267ee1795611
AndrewJP01 <andrewpotter300@gmail.com>
2025-01-15 14:23:20 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numRolls = 0;
+
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+
+        numRolls = int.Parse(Console.ReadLine());
+    }
+}
