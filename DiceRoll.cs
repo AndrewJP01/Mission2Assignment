baacdbd336233ef9b849ab081ab676536c6f3a67
AndrewJP01 <andrewpotter300@gmail.com>
2025-01-17 10:30:14 -0700

    made more of the program

--- DiceRoll.cs
+++ DiceRoll.cs
@@ -8,6 +8,32 @@ namespace Mission2Assignment
 {
     internal class DiceRoll
     {
+        int[] rollNumbers = new int[11]; //array that holds the roll counts
+        public DiceRoll(int numRolls)
+        {
 
+
+
+            for (int i = 0; i < numRolls; i++)
+            {
+                Random random = new Random();  // Create a Random object
+                int randomNumber1 = random.Next(1, 7); //random number between 1 and 6 for dice 1
+                int randomNumber2 = random.Next(1, 7); //random number between 1 and 6 for dice 2
+
+                int rollCombo = randomNumber1 + randomNumber2;
+
+                rollNumbers[rollCombo - 2]++;
+
+            }
+  
+        }
+
+        // Method to return the roll numbers array
+        public int[] GetRollNumbers()
+        {
+            return rollNumbers;  // Return the rollNumbers array
+        }
     }
+
+
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
-﻿internal class Program
+﻿using Mission2Assignment;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         int numRolls = 0;
+        
 
         Console.WriteLine("Welcome to the dice throwing simulator!");
 
@@ -10,5 +13,22 @@
 
         numRolls = int.Parse(Console.ReadLine());
         //testing git
+
+        // Pass numRolls to the DiceRoll class
+        DiceRoll diceRoll = new DiceRoll(numRolls);
+
+        // Get the roll numbers array from the DiceRoll object
+        int[] rollNumbers = diceRoll.GetRollNumbers();
+
+        // Print the tally counts for each number from 2 to 12
+        for (int i = 0; i < rollNumbers.Length; i++)
+        {
+            Console.Write($"{i + 2}: ");
+            for (int j = 0; j < ((double)rollNumbers[i] / numRolls) * 100; j++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+        }
     }
 }
