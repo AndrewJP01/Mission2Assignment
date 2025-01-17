ff653096de7990c270d516199d73535992bbb6af
AndrewJP01 <andrewpotter300@gmail.com>
2025-01-17 10:46:16 -0700

    finishing touches

--- DiceRoll.cs
+++ DiceRoll.cs
@@ -8,24 +8,22 @@ namespace Mission2Assignment
 {
     internal class DiceRoll
     {
-        int[] rollNumbers = new int[11]; //array that holds the roll counts
+        int[] rollNumbers = new int[11]; //array that holds the roll counts 2 through 12
         public DiceRoll(int numRolls)
         {
+            Random random = new Random();  // Create a Random object
 
-
-
+            // iterates the amount of rolls and does the logic
             for (int i = 0; i < numRolls; i++)
             {
-                Random random = new Random();  // Create a Random object
                 int randomNumber1 = random.Next(1, 7); //random number between 1 and 6 for dice 1
                 int randomNumber2 = random.Next(1, 7); //random number between 1 and 6 for dice 2
 
-                int rollCombo = randomNumber1 + randomNumber2;
+                int rollCombo = randomNumber1 + randomNumber2; //roll combo is the rolls of both dice
 
-                rollNumbers[rollCombo - 2]++;
+                rollNumbers[rollCombo - 2]++; //gets the roll number put in the correct spot
 
             }
-  
         }
 
         // Method to return the roll numbers array
@@ -34,6 +32,4 @@ namespace Mission2Assignment
             return rollNumbers;  // Return the rollNumbers array
         }
     }
-
-
 }
--- Program.cs
+++ Program.cs
@@ -4,23 +4,25 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        int numRolls = 0;
+        int numRolls = 0;  //varaible to hold user inputed number of rolls
         
-
-        Console.WriteLine("Welcome to the dice throwing simulator!");
-
+        Console.WriteLine("Welcome to the dice throwing simulator!\n"); 
         Console.WriteLine("How many dice rolls would you like to simulate?");
 
-        numRolls = int.Parse(Console.ReadLine());
-        //testing git
+        numRolls = int.Parse(Console.ReadLine());  //gets user input on number of rolls and makes it an integer
 
-        // Pass numRolls to the DiceRoll class
+        // Pass numRolls to the DiceRoll class with a new instance
         DiceRoll diceRoll = new DiceRoll(numRolls);
 
         // Get the roll numbers array from the DiceRoll object
         int[] rollNumbers = diceRoll.GetRollNumbers();
 
-        // Print the tally counts for each number from 2 to 12
+        //result output wording:
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1 % of the total number of rolls.");
+        Console.WriteLine("Total number of rolls = " + numRolls + ".\n");
+
+        // Print the tally counts for each number from 2 to 12 with asterisks of percentage
         for (int i = 0; i < rollNumbers.Length; i++)
         {
             Console.Write($"{i + 2}: ");
@@ -28,7 +30,9 @@ internal class Program
             {
                 Console.Write('*');
             }
-            Console.WriteLine();
+            Console.WriteLine(); //writeline for spacing
         }
+
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");    
     }
 }
